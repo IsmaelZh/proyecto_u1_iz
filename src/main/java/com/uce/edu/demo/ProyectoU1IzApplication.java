14355419327cfcaaef490d6d0e9594ecb7d6dac3
Ismael Zhindon <ismael.zhindon.xd@gmail.com>
2022-05-27 20:43:08 -0500

    Mi primer proyecto subido desde consola

---
+++ src/main/java/com/uce/edu/demo/ProyectoU1IzApplication.java
@@ -0,0 +1,22 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoU1IzApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoU1IzApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Mi primer proyecto con spring framework");
+		
+	}
+
+}
